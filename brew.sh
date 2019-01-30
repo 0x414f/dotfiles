5b8971b62417ae584af9f781a7e19e5605665fd5
Antonio de Jesus Ochoa Solano <antoniodejesusochoasolano@gmail.com>
2019-01-29 20:54:43 -0800

    Add brew.sh

---
+++ brew.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install Bash 4
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install more recent versions of some macOS tools.
+brew install nvim
+brew install grep
+brew install openssh
+brew install tmux
+brew install php
+
+# Install other useful binaries.
+brew install git
+
+# Remove outdated versions from the cellar.
+brew cleanup
