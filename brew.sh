278a1b663c952f7b84a4b7131c7df884cd42903f
Antonio de Jesus Ochoa Solano <antoniodejesusochoasolano@gmail.com>
2019-02-17 04:06:26 -0800

    brew.sh: Add qlstephen

--- brew.sh
+++ brew.sh
@@ -34,6 +34,7 @@ brew install php
 # Install other useful binaries.
 brew install git
 brew cask install quicklook-json
+brew cask install qlstephen
 
 # Remove outdated versions from the cellar.
 brew cleanup
