215ff0d0602c074a79d6cf4caccd6746571b32d9
Antonio de Jesus Ochoa-Solano <antoniodejesusochoasolano@gmail.com>
2020-05-25 01:15:24 -0700

    Set default shell

--- setup-mac.sh
+++ setup-mac.sh
@@ -4,13 +4,26 @@ set -e
 
 DIR=$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)")
 
-# Brewfile
+echo 'Bundling brew...'
 brew update
 brew bundle
 brew cleanup
+echo
 
+echo 'Linking dotfiles...'
 source setup-dotfiles.sh
+echo
 
-# Zsh
+echo 'Bundling zsh plugins...'
 antibody bundle < $DIR/.zsh_plugins > ~/.zsh_plugins
 antibody update
+echo
+
+echo 'Setting zsh default shell...'
+if ! grep -q '/usr/local/bin/zsh' '/etc/shells'; then
+  sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
+fi
+
+if [[ "$SHELL" != '/usr/local/bin/zsh' ]]; then
+  chsh -s /usr/local/bin/zsh
+fi
