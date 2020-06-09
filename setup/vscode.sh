65c44ba14d13906d343d64fb39224c15f7538ab0
Antonio de Jesus Ochoa Solano <antoniodejesusochoasolano@gmail.com>
2020-06-09 08:34:43 -0700

    More VSCode extensions

--- setup/vscode.sh
+++ setup/vscode.sh
@@ -3,13 +3,15 @@ set -e
 source setup/util.sh
 
 code \
-	--install-extension bungcip.better-toml \
+  --install-extension alefragnani.bookmarks \
+  --install-extension bungcip.better-toml \
   --install-extension dakara.dakara-foldplus \
   --install-extension eamodio.gitlens \
   --install-extension pkief.material-icon-theme \
   --install-extension rust-lang.rust \
   --install-extension serayuzgur.crates \
-  --install-extension silvenon.mdx \
+  --install-extension sleistner.vscode-fileutils \
+  --install-extension sysoev.vscode-open-in-github \
   --install-extension tomphilbin.gruvbox-themes \
   --install-extension visualstudioexptteam.vscodeintellicode \
   --install-extension vscodevim.vim
