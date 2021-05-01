fde42606bc9c36077c6c69c40b8531dc20dba286
Antonio de Jesus Ochoa Solano <antoniodejesusochoasolano@gmail.com>
2021-05-01 02:03:29 -0700

    setup/vim.sh: PlugUpgrade and force clean

--- setup/vim.sh
+++ setup/vim.sh
@@ -8,5 +8,6 @@ if [ ! -f ~/.vim/autoload/plug.vim ]; then
   curl -fLo ~/.vim/autoload/plug.vim 'https://raw.github.com/junegunn/vim-plug/master/plug.vim'
 fi
 
+nvim --headless +PlugUpgrade +qall
 nvim --headless +PlugUpdate +qall
-nvim --headless +PlugClean +qall
+nvim --headless +PlugClean! +qall
